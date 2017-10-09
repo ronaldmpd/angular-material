0b9c0d540b349588eea87e3f90ce9340692e19c3
ronaldmpd <admin@example.com>
2017-10-09 09:14:23 -0400

    add dashboard cards.

--- angular-material/src/app/dashboard/dashboard.module.ts
+++ angular-material/src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import {MatCardModule} from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule
   ],
   declarations: [DashboardComponent]
 })
