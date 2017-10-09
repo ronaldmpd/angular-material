53b641bbfd0421b7ee93f6487974ff3705a8d994
ronaldmpd <admin@example.com>
2017-10-09 09:27:02 -0400

    fix login centar and dashboard grid

--- angular-material/src/app/dashboard/dashboard.module.ts
+++ angular-material/src/app/dashboard/dashboard.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
-import {MatCardModule} from '@angular/material';
+import {MatCardModule, MatGridListModule} from '@angular/material';
 
 @NgModule({
   imports: [
